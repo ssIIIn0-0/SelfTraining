1218b242248764a46e967deae1a74cdd692c3807
ssIn00 <62862307+ssIn00@users.noreply.github.com>
2021-12-05 13:03:02 +0900

    Add files via upload

---
+++ SELF_TRAINIG/goto.js
@@ -0,0 +1,11 @@
+function goto_cal(form){
+    if(form.id.value!=""){
+        window.open('cal.html')
+    }
+}
+
+function goto_home(form){
+    if(form.id.value!=""){
+        window.open('index.html')
+    }
+}
